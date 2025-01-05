0c621ce573329eb0828a382a72bfdabb86a89c5b
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-05 18:55:43 +0530

    db connection added

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 from flask import Flask # type: ignore
-from routes.start import start  # Importing the 'start' blueprint from routes/start.py
-from routes.leaderboard import leaderboard  # Importing the 'leaderboard' blueprint from routes/leaderboard.py
+from routes.start import start  
+from routes.leaderboard import leaderboard  
+from configs.connection import create_connection
 
 app = Flask(__name__)
 
 @app.route('/')
-def backend():
-    return 'Flutter Quiz App Backend'
+def index():
+    connection = create_connection()
+    if connection:
+        return "Connected to MySQL database!"
+    else:
+        return "Failed to connect to the database."
 
 # Register Blueprints with URL prefixes
 app.register_blueprint(start, url_prefix='/start')
