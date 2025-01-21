5c0efd52e58af89ae0e09bb56c3ed959055285ea
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-21 16:35:52 +0530

    /api/question dummy added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path # type: ignore
 from leaderboard.views import leaderboard_data
+from questions.views import question_data
 
 urlpatterns = [
     path("leaderboard/", leaderboard_data),
+    path("questions/", question_data)
 ]
--- maths_quiz/settings.py
+++ maths_quiz/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "start",
     "leaderboard",
     "questions",
 ]
