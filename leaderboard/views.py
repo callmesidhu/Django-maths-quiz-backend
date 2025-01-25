67860bd6cd5dc9f456ec953d2cb81a031d61c9b6
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-26 02:52:45 +0530

    /api/leaderboard/ GET arrange by point and time

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,12 +1,12 @@
-from rest_framework.decorators import api_view #type: ignore
-from rest_framework.response import Response #type: ignore
+from rest_framework.decorators import api_view # type: ignore
+from rest_framework.response import Response   # type: ignore
 from players.models import Players
-from players.serializer import PlayerSerializer  # Import the serializer
+from players.serializer import PlayerSerializer
 
 @api_view(['GET'])
 def leaderboard_data(request):
-    # Query all players from the Players table
-    players = Players.objects.all()
+    # Query all players from the Players table and order them by points (descending) and time (ascending)
+    players = Players.objects.all().order_by('-point', 'time')
 
     # Serialize the data using the PlayerSerializer
     serializer = PlayerSerializer(players, many=True)
